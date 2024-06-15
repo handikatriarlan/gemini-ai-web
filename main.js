26b3fab22715e8672b3fb76e976d461311eb47b8
Arlan Tri Handika <handikaarlan@gmail.com>
2024-06-15 17:08:02 +0700

    add history feature

--- main.js
+++ main.js
@@ -1,46 +1,41 @@
 import { GoogleGenerativeAI, HarmBlockThreshold, HarmCategory } from "@google/generative-ai";
-import Base64 from 'base64-js';
 import MarkdownIt from 'markdown-it';
 import { maybeShowApiKeyBanner } from './gemini-api-banner';
 import './style.css';
 
-// 🔥 FILL THIS OUT FIRST! 🔥
-// 🔥 GET YOUR GEMINI API KEY AT 🔥
-// 🔥 https://g.co/ai/idxGetGeminiKey 🔥
 let API_KEY = 'AIzaSyB6kX9GWXz1ZaWrLXWeKUld1kIzLkewlTw';
 
 let form = document.querySelector('form');
 let promptInput = document.querySelector('input[name="prompt"]');
 let output = document.querySelector('.output');
 
+// Call the gemini-pro model, and get a stream of results
+const genAI = new GoogleGenerativeAI(API_KEY);
+const model = genAI.getGenerativeModel({
+  model: "gemini-pro",
+  safetySettings: [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+    },
+  ],
+});
+
+const chat = model.startChat({
+  history: [],
+  generationConfig: {
+    maxOutputTokens: 100
+  }
+});
+
 form.onsubmit = async (ev) => {
   ev.preventDefault();
   output.textContent = 'Generating...';
 
   try {
-    // Assemble the prompt by combining the text with the chosen image
-    let contents = [
-      {
-        role: 'user',
-        parts: [
-          { text: promptInput.value }
-        ]
-      }
-    ];
+    const prompt = promptInput.value;
 
-    // Call the gemini-pro model, and get a stream of results
-    const genAI = new GoogleGenerativeAI(API_KEY);
-    const model = genAI.getGenerativeModel({
-      model: "gemini-pro",
-      safetySettings: [
-        {
-          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-          threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
-        },
-      ],
-    });
-
-    const result = await model.generateContentStream({ contents });
+    const result = await chat.sendMessageStream(prompt);
 
     // Read from the stream and interpret the output as markdown
     let buffer = [];
